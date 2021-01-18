03b587c2204725041193e1cca2e5bf77c8c61ec5
ibrahim DURMAZ <idurmaz@yahoo.com>
2021-01-18 23:35:44 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Ekle(Musteri Musteri)
+        {
+            Console.WriteLine("{0} {1}, kayıtlarımıza eklendi!", Musteri.Ad, Musteri.SoyAd);
+
+        }
+
+        public void Listele(Musteri[] Musteriler)
+        {
+            Console.WriteLine("\tAdı\t\tSoyadı\t\tEposta\t\t\t\tŞehir");
+            for (int i = 0; i < Musteriler.Length; i++)
+            {
+                Console.WriteLine("\t{0}\t\t{1}\t\t{2}\t\t{3}", Musteriler[i].Ad, Musteriler[i].SoyAd, Musteriler[i].Eposta, Musteriler[i].Sehir);
+            }
+
+        }
+
+        public void Sil(Musteri Musteri)
+        {
+            Console.WriteLine("{0} {1}, adlı müşteri kaydı silindi!", Musteri.Ad, Musteri.SoyAd);
+        }
+
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Console.WriteLine("Birinci\t\tÜçüncü\n");
+            //Console.WriteLine("\tİkinvi\tDördüncü\n");
+            
+            //Müşteri bilgilerini oluşturalım
+            Musteri Musteri1 = new Musteri { ID = 1, Ad = "ibrahim", SoyAd = "Durmaz", Sehir = "Adana", Eposta = "idmfdtrtg@yahoo.com" };
+            Musteri Musteri2 = new Musteri { ID = 2, Ad = "Asi", SoyAd = "Yanak", Sehir = "Mersin", Eposta = "krlrtk@gmail.com" };
+            Musteri Musteri3 = new Musteri { ID = 3, Ad = "Vedat", SoyAd = "Major", Sehir = "İstanbul", Eposta = "vmjdghyt@hotmail.com" };
+
+            //Müşterileri diziye aktaralım
+            Musteri[] Musteriler = new Musteri[] {Musteri1, Musteri2, Musteri3};
+            
+            //Müşteriler ile işlemler yapalım
+            MusteriManager MusteriManager = new MusteriManager();
+            MusteriManager.Ekle(Musteri1);
+            MusteriManager.Ekle(Musteri2);
+
+            //Müşterileri listeleyelim
+            Console.WriteLine("\n**************************MÜŞTERİ LİSTESİ*******************************");
+            MusteriManager.Listele(Musteriler);
+
+            //Bir müşteri silme işlemi yapalım
+            Console.WriteLine("\n*********************************************************");
+            MusteriManager.Sil(Musteri2);
+
+            //Yeni bir müşteri daha oluşturalım
+            Musteri Musteri4 = new Musteri { ID = 1, Ad = "Adman", SoyAd = "Hocalı", Sehir = "Kars", Eposta = "adnmnjsuj@mymail.com" };
+            Musteriler[Musteriler.Length-1] = Musteri4;
+
+            //Müşterileri tekrar listeleyelim
+            Console.WriteLine("\n**************************MÜŞTERİ LİSTESİ*******************************");
+            MusteriManager.Listele(Musteriler);
+        }
+    }
+}
